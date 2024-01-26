918a1b3b6f33048b53f7072989b12b19fcdea1a7
alenaodinets <alena.odinecz2013@yandex.ru>
2024-01-26 14:07:20 +0400

    Class implementation and test creation

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+from pathlib import Path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -18,7 +22,6 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
-
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -34,3 +37,36 @@ class Item:
         """
         self.price = Item.pay_rate * self.price
 
+    @property
+    def name(self):
+        """Геттер для приватного атрибута name.
+        Возвращает значение приватного атрибута name."""
+        return self.__name
+
+    @name.setter
+    def name(self, item):
+        """Сеттер для приватного атрибута name.
+        Присваивает значение приватному атрибуту name.
+        Если длина переданного значения больше 10 символов, обрезает его до 10 символов."""
+        if len(item) > 10:
+            self.__name = item[:11]
+        self.__name = item
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path) -> None:
+        """класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv"""
+        cls.all.clear()
+        file_path = Path(__file__).parent.joinpath("items.csv")
+        with open(file_path, 'r', newline='', encoding='utf-8-sig') as file:
+            reader = csv.DictReader(file)
+            data = list(reader)
+            for item in data:
+                name = item['name']
+                price = cls.string_to_number(item['price'])
+                quantity = int(item['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(value: str) -> float:
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(value))
--- tests/test_item.py
+++ tests/test_item.py
@@ -5,11 +5,12 @@ from src.item import Item
 
 @pytest.fixture
 def item():
-    return Item(name="computer", price= 50000, quantity=5)
+    return Item(name="supercomputer", price=50000, quantity=5)
+
 
 def test_item_init(item):
     """Проверка наименования, цены и количества товара"""
-    assert item.name == "computer"
+    assert item.name == "supercomputer"
     assert item.price == 50000
     assert item.quantity == 5
 
@@ -18,8 +19,16 @@ def test_calculate_total_price(item):
     """Проверка подсчета общем стоимости конкретного товара"""
     assert item.calculate_total_price() == 250000
 
+
 def test_apply_discount(item):
     """Проверка новой цены с учетом скидки"""
     Item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 40000
+
+
+def test_string_to_number(item):
+    """Проверка на перевед с str в int"""
+    assert item.string_to_number('5') == 5
+    assert item.string_to_number('5.0') == 5
+    assert item.string_to_number('5.5') == 5
